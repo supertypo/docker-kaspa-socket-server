8f04b078a209960404e1d759f3381f98b0a8d88f
Marius <suprtypo@pm.me>
2023-09-01 07:59:11 +0200

    Multi arch build script for kaspa-socket-server

---
+++ Dockerfile
@@ -0,0 +1,64 @@
+##
+# builder image
+##
+FROM python:3.10-slim AS builder
+
+ARG REPO_DIR
+
+ENV APP_USER=api \
+  APP_UID=55747 \
+  APP_DIR=/app
+
+RUN apt-get update && \
+  apt-get install -y --no-install-recommends \
+    gcc \
+    libc6-dev \
+    libpq-dev
+
+RUN pip install \
+  pipenv
+
+RUN groupadd -r -g $APP_UID $APP_USER && \
+  useradd -d $APP_DIR -r -m -s /sbin/nologin -g $APP_USER -u $APP_UID $APP_USER
+
+WORKDIR $APP_DIR
+USER $APP_USER
+
+COPY --chown=$APP_USER:$APP_USER "$REPO_DIR" $APP_DIR
+
+RUN pipenv install --deploy -v
+RUN rm -r .cache/
+
+##
+# kaspa-db-filler image
+##
+FROM python:3.10-slim
+
+ENV APP_USER=api \
+  APP_UID=55747 \
+  APP_DIR=/app
+
+ENV KASPAD_HOST_1=kaspad:16110 \
+  SQL_URI=postgresql+asyncpg://postgres:password@postgresql:5432/postgres
+
+RUN apt-get update && \
+  apt-get install -y --no-install-recommends \
+    libpq-dev \
+    dumb-init && \
+  rm -rf /var/lib/apt/lists/*
+
+RUN pip install \
+  pipenv
+
+RUN groupadd -r -g $APP_UID $APP_USER && \
+  useradd -d $APP_DIR -r -m -s /sbin/nologin -g $APP_USER -u $APP_UID $APP_USER
+
+WORKDIR $APP_DIR
+USER $APP_USER
+
+ENTRYPOINT ["/usr/bin/dumb-init", "--"]
+
+CMD pipenv run gunicorn -b 0.0.0.0:8000 -w 1 -k uvicorn.workers.UvicornWorker main:app --timeout 120
+
+COPY --chown=$APP_USER:$APP_USER --from=builder $APP_DIR $APP_DIR
+
---
+++ build.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+DOCKER_REPO=supertypo/kaspa-socket-server
+ARCHES="linux/amd64 linux/arm64"
+
+BUILD_DIR="$(dirname $0)"
+PUSH=$1
+TAG=${2:-main}
+REPO_URL="https://github.com/lAmeR1/kaspa-socket-server.git"
+REPO_DIR="work/kaspa-socket-server"
+
+set -e
+
+if [ ! -d "$BUILD_DIR/$REPO_DIR" ]; then
+  git clone "$REPO_URL" "$BUILD_DIR/$REPO_DIR"
+  echo $(cd "$BUILD_DIR/$REPO_DIR" && git reset --hard HEAD~1)
+fi
+
+echo "===================================================="
+echo " Pulling $REPO_URL"
+echo "===================================================="
+
+(cd "$BUILD_DIR/$REPO_DIR" && git fetch && git checkout $TAG && (git pull 2>/dev/null | true))
+
+tag=$(cd "$BUILD_DIR/$REPO_DIR" && git log -n1 --format="%cs.%h")
+
+docker=docker
+id -nG $USER | grep -qw docker || docker="sudo $docker"
+
+plain_build() {
+  echo
+  echo "===================================================="
+  echo " Running current arch build"
+  echo "===================================================="
+  $docker build --pull \
+    --build-arg REPO_DIR="$REPO_DIR" \
+    --tag $DOCKER_REPO:$tag "$BUILD_DIR"
+
+  $docker tag $DOCKER_REPO:$tag $DOCKER_REPO:multi-arch
+  echo Tagged $DOCKER_REPO:multi-arch
+
+  if [ "$PUSH" = "push" ]; then
+    $docker push $DOCKER_REPO:$tag
+    $docker push $DOCKER_REPO:multi-arch
+  fi
+  echo "===================================================="
+  echo " Completed current arch build"
+  echo "===================================================="
+}
+
+multi_arch_build() {
+  echo
+  echo "===================================================="
+  echo " Running multi arch build"
+  echo "===================================================="
+  dockerRepoArgs=
+  if [ "$PUSH" = "push" ]; then
+    dockerRepoArgs="$dockerRepoArgs --push"
+  fi
+  $docker buildx build --pull --platform=$(echo $ARCHES | sed 's/ /,/g') $dockerRepoArgs \
+    --build-arg REPO_DIR="$REPO_DIR" \
+    --tag $DOCKER_REPO:$tag \
+    --tag $DOCKER_REPO:multi-arch "$BUILD_DIR"
+  echo "===================================================="
+  echo " Completed multi arch build"
+  echo "===================================================="
+}
+
+if [ "$PUSH" = "push" ]; then
+  echo
+  echo "===================================================="
+  echo " Setup multi arch build ($ARCHES)"
+  echo "===================================================="
+  if $docker buildx create --name=mybuilder --append --node=mybuilder0 --platform=$(echo $ARCHES | sed 's/ /,/g') --bootstrap --use 1>/dev/null 2>&1; then
+    echo "SUCCESS - doing multi arch build"
+    multi_arch_build
+  else
+    echo "FAILED - building on current arch"
+    plain_build
+  fi
+else
+  plain_build
+fi
+
